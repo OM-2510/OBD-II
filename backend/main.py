9ae7736a17c0e58695fffc546385eeb3a6018126
OM-2510 <omdeep779@gmail.com>
2026-07-28 17:32:45 +0530

    Replaced eventlet with gevent

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,13 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 from flask import Flask
 from flask_socketio import SocketIO
+import gevent
 import obd
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=60) 
+socketio = SocketIO(app, async_mode="gevent", cors_allowed_origins="*", ping_timeout=60) 
 
 running = False
 connection = None
@@ -14,14 +15,14 @@ connection = None
 
 def obd_reader():
     global running, connection
-    print("Trying to connect with Vehicle ... ")
+    print("Trying to connect with Vehicle ...")
     
     try:
         if connection is None or not connection.is_connected():
             connection = obd.OBD("COM4", fast=False, timeout=15)
         
         if connection.is_connected():
-            print("Successful connection with vehicle ✅")
+            print("Vehicle Connected✅")
 
             while running:
                 rpm = connection.query(obd.commands.RPM)
@@ -34,9 +35,7 @@ def obd_reader():
                 timing = connection.query(obd.commands.TIMING_ADVANCE)
                 eninge_load = connection.query(obd.commands.ENGINE_LOAD)
                 stft = connection.query(obd.commands.SHORT_FUEL_TRIM_1)
-                ltft = connection.query(obd.commands.LONG_FUEL_TRIM_1)
-
-                
+                ltft = connection.query(obd.commands.LONG_FUEL_TRIM_1)               
 
                 payload = {
                     "RPM": rpm.value.magnitude if not rpm.is_null() else 0,
@@ -50,40 +49,40 @@ def obd_reader():
                     "ENGINE_LOAD" : eninge_load.value.magnitude if not eninge_load.is_null() else 0,
                     "STFT" : stft.value.magnitude if not stft.is_null() else None,
                     "LTFT" : ltft.value.magnitude if not ltft.is_null() else None,
-
-
                 }
                 
                 socketio.emit('obd_data', payload)
-                eventlet.sleep(0.05) 
+                gevent.sleep(0.05) 
         else:
-            print("Failed conneciton with vehicle ❌")
+            print("Vehicle Connection Failed❌")
             running = False
             
     except Exception as e:
-        print(f"Connection error: {e}")
+        print(f"Vehicle Connection error: {e}")
         running = False
 
+
+### Webscokets Events
 @socketio.on('connect')
 def handle_connect():
     global running
-    print("Browser Chained 🔗")
+    print("WebSockets Hot🔌")
     if not running:
         running = True
         socketio.start_background_task(obd_reader)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print("Browser Unlinked ⛓️‍💥")
-    print("Searching for Signal ... ")
+    print("Websockets Down 🛑")
+    print("Retrying... ")
     socketio.start_background_task(shutdown)
 
 def shutdown():
-    eventlet.sleep(15)
+    gevent.sleep(15)
     if len(socketio.server.eio.sockets) == 0:
         global running
         running = False
-        print("Server Down 🚩 ...")
+        print("Idle Payload at Halt 💤") 
 
 
 
